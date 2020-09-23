2882ed160bb5ea4b7ac9f151b79d94b560d62565
l0wlit0 <ruisrsilva@gmail.com>
2020-09-23 11:52:32 +0100

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -24,5 +25,10 @@ public class Program {
         System.out.println("\n===== Test 3: seller findAll =====");
         list = sellerDao.findAll();
         list.forEach(System.out::println);
+
+        System.out.println("\n===== Test 4: seller insert =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
